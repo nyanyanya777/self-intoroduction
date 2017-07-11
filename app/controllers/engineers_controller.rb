ade2f6f7752fa487d6749a359dcb9d1efb4b3785
kohei <kohei@koheinoMacBook-Pro.local>
2017-07-11 14:45:26 +0900

    デプロイ前チェック

--- app/controllers/engineers_controller.rb
+++ app/controllers/engineers_controller.rb
@@ -1,5 +1,5 @@
 class EngineersController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_other, only: [:show, :update, :delete, :edit]
 
   def index
